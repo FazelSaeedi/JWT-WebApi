1931fd1129ff0cd38c76ca003508f0f1b5bfa6f4
Fazel Saeedi <fazels36@gmail.com>
2021-03-30 15:34:21 +0430

    Account  & Role Application Created

---
+++ Solution1/AccountManagement.Application/AccountApplication.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using _0_Framework.Application;
+using AccountManagement.Application.Contracts.Account;
+
+
+namespace AccountManagement.Application
+{
+    public class AccountApplication : IAccountApplication
+    {
+        public AccountViewModel GetAccountBy(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationResult Register(RegisterAccount command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationResult Edit(EditAccount command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationResult ChangePassword(ChangePassword command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public OperationResult Login(Login command)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public EditAccount GetDetails(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<AccountViewModel> GetAccounts()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
