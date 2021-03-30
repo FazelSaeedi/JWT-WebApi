82b4a45fb252bba346691ebde88fde39e8c6c942
Fazel Saeedi <fazels36@gmail.com>
2021-03-30 15:06:55 +0430

    Role & Account & Permition Domain implemented

---
+++ Solution1/AccountManagement.Domain/AccountAgg/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountManagement.Domain.RoleAgg;
+
+namespace AccountManagement.Domain.AccountAgg
+{
+    public class Account
+    {
+ 
+        public long Id { get; private set; }
+        public string Fullname { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Mobile { get; private set; }
+        public string ProfilePhoto { get; private set; }
+        public DateTime CreataionDate { get; private set; }
+
+
+        public long RoleId { get; private set; }
+        public Role Role { get; private set; }
+
+        public Account(long id, string fullname, string username, string password, string mobile, string profilePhoto, long roleId)
+        {
+            Id = id;
+            Fullname = fullname;
+            Username = username;
+            Password = password;
+            Mobile = mobile;
+            ProfilePhoto = profilePhoto;
+
+            if (roleId == 0)
+                RoleId = 2;
+
+            CreataionDate = DateTime.Now;
+
+        }
+
+        public void Edit(long id, string fullname, string username, string password, string mobile, string profilePhoto, long roleId)
+        {
+            Id = id;
+            Fullname = fullname;
+            Username = username;
+            Password = password;
+            Mobile = mobile;
+
+            if (!string.IsNullOrWhiteSpace(profilePhoto))
+                ProfilePhoto = profilePhoto;
+
+            if (roleId == 0)
+                RoleId = 2;
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+        }
+    }
+}
---
+++ Solution1/AccountManagement.Domain/RoleAgg/Role.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountManagement.Domain.AccountAgg;
+
+namespace AccountManagement.Domain.RoleAgg
+{
+    public class Role
+    {
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public List<Permission> Permissions { get; private set; }
+        public List<Account> Accounts { get; private set; }
+
+
+        public DateTime CreataionDate { get; private set; }
+
+
+        protected Role()
+        {
+        }
+
+        public Role(string name, List<Permission> permissions )
+        {
+            Name = name;
+            Permissions = permissions;
+            Accounts = new List<Account>();
+            CreataionDate = DateTime.Now;
+        }
+
+        public void Edit(string name, List<Permission> permissions)
+        {
+            Name = name;
+            Permissions = permissions;
+        }
+    }
+}
